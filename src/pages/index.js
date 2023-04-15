5af54ef3e52475236fa25a5992601bbaedceadd3
Cedric Tan <cedrictys@infra04-wg013.eduroam.wireless.umn.edu>
2023-04-15 15:36:58 -0500

    added some files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head"
 import Link from "next/link"
 import styles from '@/styles/Home.module.css'
-import { ClerkProvider, SignUp, SignIn, SignedOut, SignedIn, UserButton } from '@clerk/nextjs'
+import { SignedOut, SignedIn} from '@clerk/nextjs'
 
 
 export default function Home() {
@@ -11,16 +11,15 @@ export default function Home() {
         <title>Home</title>
       </Head>
       <main className={styles.main}>
-        <br></br>
         Welcome to the to-do list,
         <br></br>
         personalize for you.
         <br></br>
         <SignedIn>
-          <button className={styles.button}><Link href="/todo">To-do</Link></button>
+          <button className={styles.button}><Link href="/todo" style={{ textDecoration: 'none', color: 'black' }}>To-do</Link></button>
         </SignedIn>
         <SignedOut>
-          <button className={styles.button}><Link href="/login">Login</Link></button>
+          <button className={styles.button}><Link href="/login" style={{ textDecoration: 'none', color: 'black' }}>Login</Link></button>
         </SignedOut>
       </main>
     </>
